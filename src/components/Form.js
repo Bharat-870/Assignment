bf6bd325cac9b4e5938fd38e67b7a4001c5601ea
Bharat Singh <87543456+Bharat-870@users.noreply.github.com>
2022-01-06 16:16:50 +0530

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -32,22 +32,16 @@ function Form() {
         const errors = {};
         //const re = /^\S*$/;
         const regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-        if (!values.phonenumber) {
-            errors.phonenumber = 'Mobile Number is required!';
-        } else if (values.phonenumber.length < 10) {
+        if (!values.phonenumber || values.phonenumber.length < 10) {
             errors.phonenumber = 'Please enter a valid mobile number'
         } else if (values.phonenumber.length >10) {
             errors.phonenumber = 'Please enter a valid mobile number'
         }
-        if (!values.username) {
-            errors.username = 'User Name is required!';
-        } else if (values.username.length < 3 ) {
+        if (!values.username || values.username.length < 3 ) {
             // || !re.test(values.username)
             errors.username = 'Please enter a valid name'
         }
-        if (!values.email) {
-            errors.email = 'E-mail is required!';
-        } else if (!regex.test(values.email)) {
+        if (!values.email || !regex.test(values.email)) {
             errors.email = 'Please enter a valid e-mail id'
         }
         return errors;
@@ -72,29 +66,34 @@ function Form() {
             <div id='secondDiv'>
 
                 <form id='form' onSubmit={handleSubmit}>
-                    <p id='formHeading' className='clr'>Book a Free Demo Class</p>
-                    <p id='warning' className='clr'>Limited Seats only!</p>
+                    <p className='formHeading' id='clr'>Book a Free Demo Class</p>
+                    <p className='warning' id='clr'>Limited Seats only!</p>
 
                     {/* <label className='clr' id='floatingLabel'>Mobile Number</label> */}
-                    <input className='clr' type="tel" name='phonenumber' autoComplete='off' placeholder='Enter Mobile Number' id="mobileNumber" value={formValues.phonenumber} onChange={handleChange} />
-                    <p className='clr' id='errormsg'>{ formErrors.phonenumber }</p>
+                    <input id='clr' type="tel" name='phonenumber' autoComplete='off' placeholder='Enter Mobile Number' className="mobileNumber" value={formValues.phonenumber} onChange={handleChange} />
+                    <span className='plcehldr'>Mobile Number</span>
+                    <br/>
+                    <p id='clr' className='errormsg'>{ formErrors.phonenumber }</p>
                     <br></br>
                     {/* <label>Mobile Number</label> */}
-                    <input className='clr' type="text" name='username' autoComplete='off' placeholder='Enter Full Name' id="name" value={formValues.username} onChange={handleChange} />
-                    <p className='clr' id='errormsg'>{ formErrors.username }</p>
+                    <input id='clr' type="text" name='username' placeholder='Enter Full Name' autoComplete='off' className="name" value={formValues.username} onChange={handleChange} />
+                    <span className='plcehldr'> Full Name </span>
+                    <br/>
+                    <p id='clr' className='errormsg'>{ formErrors.username }</p>
                     <br></br>
                     {/* <label>Mobile Number</label> */}
-                    <input className='clr' type="text" name='email' autoComplete='off' placeholder='Enter E-mail ID' id="email" value={formValues.email} onChange={handleChange} />
-                    <p className='clr' id='errormsg'>{ formErrors.email }</p>
-                    <br></br>
-                    <label id='thirdLine' className='clr'>What describes you best?</label>
-                    <br></br>
-                    <select required className='clr' autoComplete='off' placeholder='Select options' id="selectOption">
-                        <option id='entries' className='clr' value="" disabled selected hidden>Select option</option>
-                        <option id='entries' className='clr' value="College Student">College Student</option>
-                        <option id='entries' className='clr' value="Working Professional">Working Professional</option>
-                        <option id='entries' className='clr' value="Full time aspirant">Full time aspirant</option>
-                        <option id='entries' className='clr' value="Other">Other</option>
+                    <input id='clr' type="text" name='email' autoComplete='off' placeholder='Enter E-mail ID' className="email" value={formValues.email} onChange={handleChange} />
+                    <span className='plcehldr'>E-mail ID</span>
+                    <br/>
+                    <p id='clr' className='errormsg'>{ formErrors.email }</p>
+                    <br/>
+                    <label className='thirdLine' id='clr'>What describes you best?</label>
+                    <select required id='clr' autoComplete='off' placeholder='Select options' className="selectOption">
+                        <option className='entries' id='clr' value="" disabled selected hidden>Select option</option>
+                        <option className='entries' id='clr' value="College Student">College Student</option>
+                        <option className='entries' id='clr' value="Working Professional">Working Professional</option>
+                        <option className='entries' id='clr' value="Full time aspirant">Full time aspirant</option>
+                        <option className='entries' id='clr' value="Other">Other</option>
                     </select>
                     <br></br>   
                     {/* <Link to={'/nextpage'}> */}
@@ -108,7 +107,7 @@ function Form() {
                     }
                     {/* </Link>  */}
                     <br></br>
-                    <p className='clr' id='lastLine'>By clicking 'Submit' button,you explicitly solicit a call &<br/> email from uFaber</p>
+                    <p id='clr' className='lastLine'>By clicking 'Submit' button,you explicitly solicit a call &<br/> email from uFaber</p>
                 </form>
             </div>
         </div>
